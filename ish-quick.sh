c6aad714d5dea3c985e2f040702bf29418164c58
gleekr <gleeky@tuta.io>
2026-05-30 08:16:49 -0700

    cleanup: remove iSH setup scripts (no longer needed)

--- ish-quick.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# One-liner iSH setup (paste into terminal):
-# curl -fsSL https://raw.githubusercontent.com/gleekr/enxr/master/ish-quick.sh | sh
-
-echo "http://mirror.math.princeton.edu/pub/alpinelinux/latest-stable/main" > /etc/apk/repositories
-echo "http://mirror.math.princeton.edu/pub/alpinelinux/latest-stable/community" >> /etc/apk/repositories
-apk update && apk add --no-cache git python3.10 py3.10-pip ffmpeg && pip3.10 install -q yt-dlp && echo "[OK] ready"
