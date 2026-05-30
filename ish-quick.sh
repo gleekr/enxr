ea608f184bdc73e3f28f8fb28be275c9932717cb
gleekr <gleeky@tuta.io>
2026-05-30 00:38:57 -0700

    scripts: require Python 3.11 (3.10+)
    Use python3.11 and py3.11-pip explicitly in iSH setup

--- ish-quick.sh
+++ ish-quick.sh
@@ -4,4 +4,4 @@
 
 echo "http://mirror.math.princeton.edu/pub/alpinelinux/latest-stable/main" > /etc/apk/repositories
 echo "http://mirror.math.princeton.edu/pub/alpinelinux/latest-stable/community" >> /etc/apk/repositories
-apk update && apk add --no-cache git python3 py3-pip ffmpeg && pip3 install -q yt-dlp && echo "[OK] ready"
+apk update && apk add --no-cache git python3.11 py3.11-pip ffmpeg && pip3.11 install -q yt-dlp && echo "[OK] ready"
